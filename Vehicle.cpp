fa4b93fbeb25adaa2487aadb4c001edd8206ac4e
TimmyMinhDuc <160477197+TimmyMinhDuc@users.noreply.github.com>
2024-05-06 14:41:41 +0700

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,65 +1,58 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 class Vehicle {
-private:
-  string vehicleID;
-  string brand;
-  string model;
-  int year;
-  string color;
-
-public:
-	// Constructor
-  Vehicle(string id, string brand, string model, int year, string color) {
-    this->vehicleID = id;
-    this->brand = brand;
-    this->model = model;
-    this->year = year;
-    this->color = color;
-  }
-
-	// Getter
-  string getVehicleID() const { 
-	  return vehicleID; }
-  string getBrand() const { 
-	  return brand; }
-  string getModel() const { 
-	  return model; }
-  int getYear() const { 
-	  return year; }
-  string getColor() const { 
-	  return color; }
-
-	// Setter
-  void displayInfo() const {
-    cout << "Vehicle ID: " << getVehicleID() << endl;
-    cout << "Brand: " << getBrand() << endl;
-    cout << "Model: " << getModel() << endl;
-    cout << "Year: " << getYear() << endl;
-    cout << "Color: " << getColor() << endl;
-  }
-
-	// Kiem tra tuoi xe
-  bool isVintage() const {
-    return (2024 - year) >= 25; 
-  }
+	private:
+		string vehicleID;
+		string brand;
+		string model;
+		int year;
+		string color;
+		
+	public:
+		Vehicle(string id, string brand, string model, int year, string color) {
+		this->vehicleID = id;
+    		this->brand = brand;
+    		this->model = model;
+    		this->year = year;
+   		this->color = color;
+		}
+		
+	string getVehicleID() const {
+		return vehicleID; }
+	string getBrand() const {
+		return brand; }
+	string getModel() const {
+		return model; }
+	int getYear() const {
+		return year; }
+	string getColor() const {
+		return color; }
+		
+	void displayInfo() const {
+	cout << "ID Xe: " << getVehicleID() << endl; 
+	cout << "Hang xe: " << getBrand() << endl;
+	cout << "Doi xe: " << getModel() << endl;
+	cout << "Nam san xuat: " << getYear() << endl;
+	cout << "Mau xe: " << getColor() << endl;
+	}
+	
+	bool isVintage() const {
+		return (2024 - year) >= 25; }
 };
 
 int main() {
-  // Tao doi tuong Mercedes-Benz
-  Vehicle mercedes("GB9923", "Mercedes-Benz", "Double-Gentle-Class", 2023, "Light Silver");
-
-  // In thông tin ve xe Mercedes-Benz
-  mercedes.displayInfo();
-
-  // Kiem tra xem co phai xe cu khong
-  if (mercedes.isVintage()) {
+	Vehicle mercedes("MD69", "Mercedes-Benz", "SSS-Vip", 2023, "Bac Soi Trang");
+	mercedes.displayInfo();
+	
+	if (mercedes.isVintage()) {
     cout << mercedes.getBrand() << " " << mercedes.getVehicleID() << " la xe cu." << endl;
-  } 
+  	} 
   	else {
     cout << mercedes.getBrand() << " " << mercedes.getVehicleID() << " khong la xe cu." << endl;
-  }
+  	}
 
-  return 0;
+return 0;
 }

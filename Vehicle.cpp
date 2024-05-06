9e8cd6355f1c450db9a6bba9d7610db16c701d38
TimmyMinhDuc <160477197+TimmyMinhDuc@users.noreply.github.com>
2024-05-06 15:15:20 +0700

    Update Vehicle.cpp

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -13,30 +13,41 @@ class Vehicle {
 		
 	public:
 		Vehicle(string id, string brand, string model, int year, string color) {
-		this->vehicleID = id;
+			this->vehicleID = id;
     		this->brand = brand;
     		this->model = model;
     		this->year = year;
-   		this->color = color;
+   			this->color = color;
 		}
 		
 	string getVehicleID() const {
 		return vehicleID; }
+		
 	string getBrand() const {
 		return brand; }
+		
 	string getModel() const {
 		return model; }
+		
 	int getYear() const {
 		return year; }
+		
 	string getColor() const {
 		return color; }
 		
+		
 	void displayInfo() const {
+		
 	cout << "ID Xe: " << getVehicleID() << endl; 
+	
 	cout << "Hang xe: " << getBrand() << endl;
+	
 	cout << "Doi xe: " << getModel() << endl;
+	
 	cout << "Nam san xuat: " << getYear() << endl;
+	
 	cout << "Mau xe: " << getColor() << endl;
+	
 	}
 	
 	bool isVintage() const {
@@ -45,14 +56,19 @@ class Vehicle {
 
 int main() {
 	Vehicle mercedes("MD69", "Mercedes-Benz", "SSS-Vip", 2023, "Bac Soi Trang");
+	
 	mercedes.displayInfo();
 	
 	if (mercedes.isVintage()) {
-    cout << mercedes.getBrand() << " " << mercedes.getVehicleID() << " la xe cu." << endl;
+		
+    cout << mercedes.getBrand() << " " << mercedes.getVehicleID() << " la xe cu" << endl;
   	} 
+  	
   	else {
-    cout << mercedes.getBrand() << " " << mercedes.getVehicleID() << " khong la xe cu." << endl;
+  		
+    cout << mercedes.getBrand() << " " << mercedes.getVehicleID() << " khong la xe cu" << endl;
   	}
 
 return 0;
+
 }
